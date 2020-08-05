0019d26cdf0a5b4ee1550fb144d16510482200b4
debashis naskar <debanaskar13@gmail.com>
2020-08-05 17:55:22 +0530

    add digital clock

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 setInterval(setClock,1000);
 
-const hourHand=document.querySelector('[data-hour-hand]')
-const minuteHand=document.querySelector('[data-minute-hand]')
-const secondHand=document.querySelector('[data-second-hand]')
-console.log(hourHand)
+const hourHand=document.querySelector('[data-hour-hand]');
+const minuteHand=document.querySelector('[data-minute-hand]');
+const secondHand=document.querySelector('[data-second-hand]');
+let hour = document.getElementById('hour');
+let minute = document.getElementById('minute');
+let second = document.getElementById('second');
+let ampm = document.getElementById('ampm');
 
 function setClock(){
     const currentDate=new Date();
     const secondsRatio=currentDate.getSeconds() / 60;
     const minutesRatio=(secondsRatio + currentDate.getMinutes()) / 60;
     const hoursRatio=(minutesRatio + currentDate.getHours()) / 12;
-    setRotation(secondHand, secondsRatio)
-    setRotation(minuteHand, minutesRatio)
-    setRotation(hourHand, hoursRatio)
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+
+    let currentHour = currentDate.getHours();
+    let currentMinute = currentDate.getMinutes();
+    let currentSecond = currentDate.getSeconds();
+
+    if (currentHour>12){
+        currentHour = currentHour-12;
+        ampm.innerHTML = 'PM';
+    }
+    else{
+        ampm.innerHTML = 'AM';
+    }
+
+    hour.innerHTML = currentHour;
+    minute.innerHTML = currentMinute;
+    second.innerHTML = currentSecond;
 
 }
 
